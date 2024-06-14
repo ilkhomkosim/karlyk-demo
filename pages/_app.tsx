581ccf5b3accdf93d7a49bd5877b5fab4aa5a3f7
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-06-14 16:38:54 +0900

    feat: develop apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,24 @@ import { ThemeProvider } from "@emotion/react";
 import { CssBaseline, createTheme } from "@mui/material";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
   const [theme, setTheme] = useState(createTheme(light));
 
-  // Socket.io, Redux, Mui
+  // Socket.io, Redux, Mui, Apollo client
   return (
+    <ApolloProvider client={client}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
+    </ApolloProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,34 @@ import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
+import { log } from "console";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "propertyRank",
+        direction: "DSC",
+        search:{}
+        
+    }
+    }
+  });
+
+  console.log("getPropertiesData:", getPropertiesData);
+  
 
   if(device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>
